a2d1b6f6670d24b06a037e2e08df944e3d0101f4
GamingHazard <bb466882@gmail.com>
2024-08-18 15:37:21 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,161 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+const nodemailer = require("nodemailer");
+const ws = require("ws");
+require("dotenv").config();
+
+const app = express();
+const port = 3000;
+const cors = require("cors");
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const jwt = require("jsonwebtoken");
+
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error Connecting to MongoDB");
+  });
+
+// Create the HTTP server
+const server = app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// Create the WebSocket server
+const wss = new ws.Server({ server });
+
+// WebSocket connection handling
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+
+  ws.on("message", (message) => {
+    console.log("Received:", message);
+    // Handle incoming messages and broadcast them if necessary
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
+const User = require("./models/user");
+
+// Endpoint to register a user
+app.post("/register", async (req, res) => {
+  try {
+    const { username, email, phone, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already registered" });
+    }
+
+    const newUser = new User({ username, email, phone, password });
+    newUser.verificationToken = crypto.randomBytes(20).toString("hex");
+
+    await newUser.save();
+    sendVerificationEmail(newUser.email, newUser.verificationToken);
+
+    res.status(200).json({ message: "Registration successful" });
+  } catch (error) {
+    console.log("Error registering user", error);
+    res.status(500).json({ message: "Error registering user" });
+  }
+});
+
+const sendVerificationEmail = async (email, verificationToken) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: "Uga-Cycle",
+    to: email,
+    subject: "Email Verification",
+    text: `Please click the following link to verify your email: https://waste-recycle-app-backend.onrender.com/verify/${verificationToken}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    console.log("Error sending email", error);
+  }
+};
+
+app.get("/verify/:token", async (req, res) => {
+  try {
+    const token = req.params.token;
+    const user = await User.findOne({ verificationToken: token });
+    if (!user) {
+      return res.status(404).json({ message: "Invalid token" });
+    }
+
+    user.verified = true;
+    user.verificationToken = undefined;
+    await user.save();
+
+    res.status(200).json({ message: "Email verified successfully" });
+  } catch (error) {
+    console.log("Error verifying token", error);
+    res.status(500).json({ message: "Email verification failed" });
+  }
+});
+
+const generateSecretKey = () => {
+  return crypto.randomBytes(32).toString("hex");
+};
+
+const secretKey = generateSecretKey();
+
+// Endpoint for logging in
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "Invalid email" });
+    }
+
+    if (user.password !== password) {
+      return res.status(404).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, secretKey);
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: "Login failed" });
+  }
+});
+
+// Endpoint to get user profile
+app.get("/profile/:userId", async (req, res) => {
+  try {
+    const userId = req.params.userId;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    return res.status(200).json({ user });
+  } catch (error) {
+    res.status(500).json({ message: "Error while getting the profile" });
+  }
+});
---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  email: { type: String, required: true, unique: true },
+  phone: { type: Number, required: true, unique: true },
+  password: { type: String, required: true },
+  profilePicture: { type: String },
+  joindDate: { type: Date, default: Date.now },
+  sentFollowRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  receivedFollowRequests: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  ],
+
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+  verificationToken: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
