6fa3698b74ea985fefc4848af6a833ba4b7a8880
GamingHazard <bb466882@gmail.com>
2024-08-19 03:24:25 -0700

    first commit

--- models/user.js
+++ models/user.js
@@ -1,20 +1,17 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: {
+  username: {
     type: String,
     unique: true,
     required: true,
   },
   email: { type: String, required: true, unique: true },
+  phone: { type: Number, required: true, unique: true },
   password: { type: String, required: true },
   profilePicture: { type: String },
   joindDate: { type: Date, default: Date.now },
-  sentFollowRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  receivedFollowRequests: [
-    { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  ],
-  followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+
   verified: {
     type: Boolean,
     default: false,
