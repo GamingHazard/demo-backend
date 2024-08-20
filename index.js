2148a64101a70a485e8dd07b3ac554facd90be20
GamingHazard <bb466882@gmail.com>
2024-08-20 14:15:21 -0700

    update

--- index.js
+++ index.js
@@ -172,7 +172,7 @@ app.post("/login", async (req, res) => {
 });
 
 // Endpoint to get user profile
-app.get("/profile", async (req, res) => {
+app.get("/profile/:userId", async (req, res) => {
   try {
     const userId = req.params.userId;
 
